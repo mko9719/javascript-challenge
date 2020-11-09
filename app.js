b51aa45de038044272ec5c3e4503a63484430d94
mko9719 <67659135+mko9719@users.noreply.github.com>
2020-11-09 14:28:57 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
 // Assign the data from `data.js` to a descriptive variable
-var sightings = data;
+// var sightings = data;
 
 // Select the button
-var button = d3.select("#button");
+var button = d3.select("button");
 
 // Select the form
-var form = d3.select("#form");
+var form = d3.select("form");
 var tbody = d3.select("tbody")
 var table = d3.select("table")
 
 // Create event handlers 
-button.on("click", runEnter);
+filter_button.on("click", runEnter);
 form.on("submit",runEnter);
 
 // Complete the event handler function for the form
 function runEnter() {
 
-  // Prevent the page from refreshing
   // jQuery method for emptying the existing table each time
     // a new query is performed
     $("#tbody").empty();
 
-  
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("input");
+    // Select the input element and get the HTML element
+    var inputElement = d3.select("input");
+    
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
-  var filteredData = data.filter(data => data.datetime === inputValue);
-  d3.event.preventDefault();
-  filteredData.forEach((UFO_Sightings) => {
-      var row = tbody.append("tr");
-      Object.entries(UFO_Sightings).forEach(([key, value]) => {
-          var cell = row.append("td");
-          cell.text(value);
-      });
-  });
-    // jQuery method for emptying the input field after click
+    // Match input date with value
+    var filteredData = data.filter(data => data.datetime === inputValue);
+
+    // Prevent Refresh
+    d3.event.preventDefault();
+    
+    filteredData.forEach((UFO_Sightings_filter) => {
+        var row = tbody.append("tr");
+        Object.entries(UFO_Sightings_filter).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+    // Emptying the input field after click
     $("#datetime").val('');
 };
 
